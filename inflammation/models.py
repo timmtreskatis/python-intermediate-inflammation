fa032920c2aa0c226fd9c37ab0786e399c57d357
Timm Treskatis <timm.treskatis@tu-dortmund.de>
2025-03-13 18:18:13 +0100

    Add function to normalise patient data

--- inflammation/models.py
+++ inflammation/models.py
@@ -46,3 +46,9 @@ def daily_min(data):
     :return: An array of min values of measurements for each day.
     """
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=1)
+    return data / max[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,37 +4,40 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
+
 
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
     ]
 )
 def test_daily_mean(test, expected):
     """Test that mean function works for different arrays of integers."""
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2, 3, 4], [3, 4, 5, 5], [4, 3, 2, 1] ], [4, 4, 5, 5]),
-        ([ [1, 2, 3], [3, 4, 3], [3, 2, 1] ], [3, 4, 3]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2, 3, 4], [3, 4, 5, 5], [4, 3, 2, 1]], [4, 4, 5, 5]),
+        ([[1, 2, 3], [3, 4, 3], [3, 2, 1]], [3, 4, 3]),
     ]
 )
 def test_daily_max(test, expected):
     """Test that max function works for different arrays of integers."""
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2, 3, 4], [3, 4, 5, 5], [4, 3, 2, 1] ], [1, 2, 2, 1]),
-        ([ [1, 2, 3], [3, 4, 3], [3, 2, 1] ], [1, 2, 1]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2, 3, 4], [3, 4, 5, 5], [4, 3, 2, 1]], [1, 2, 2, 1]),
+        ([[1, 2, 3], [3, 4, 3], [3, 2, 1]], [1, 2, 1]),
     ]
 )
 def test_daily_min(test, expected):
@@ -47,3 +50,18 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+         [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ]
+)
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
