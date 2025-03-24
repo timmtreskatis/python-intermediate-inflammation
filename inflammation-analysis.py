98b5774fda18262bfc1bed2a964d5c74b6ce0fad
Timm Treskatis <timm.treskatis@tu-dortmund.de>
2025-03-24 16:48:10 +0100

    Encapsulate data import

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -5,7 +5,7 @@ import argparse
 import os
 
 from inflammation import models, views
-from inflammation.compute_data import analyse_data
+from inflammation.compute_data import analyse_data, CSVDataSource
 
 
 def main(args):
@@ -21,7 +21,8 @@ def main(args):
 
 
     if args.full_data_analysis:
-        analyse_data(os.path.dirname(infiles[0]))
+        data_source = CSVDataSource(os.path.dirname(infiles[0]))
+        analyse_data(data_source)
         return
 
     for filename in infiles:
--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -8,17 +8,33 @@ import numpy as np
 from inflammation import models, views
 
 
-def analyse_data(data_dir):
+class CSVDataSource:
+    """
+    Loads all the inflammation CSV files within a specified directory.
+    """
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(
+            self.dir_path, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(
+                f"No inflammation data CSV files found in path {self.dir_path}"
+            )
+        data = map(models.load_csv, data_file_paths)
+        return list(data)
+
+
+def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
-    Gets all the inflammation data from CSV files within a directory,
-    works out the mean inflammation value for each day across all datasets,
-    then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
+    Gets all the inflammation data from a data source, works out the mean
+    inflammation value for each day across all datasets, then plots the graphs
+    of standard deviation of these means.
+    """
+    data = data_source.load_inflammation_data()
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
